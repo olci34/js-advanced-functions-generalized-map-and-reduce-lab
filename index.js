29c5956b3f6425f99d8e955ba3a8145e6a6e5086
github username <github email address>
2021-04-27 13:46:02 -0400

    commit tree f39c99de8392cf28ec867411a71ef5d298d8b1a7

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(arr, func) {
+    let result = []
+    for (let i = 0; i < arr.length; i++) {
+        result.push(func(arr[i]))
+    }
+    return result
+}
+
+function reduce(arr, callback, initialVal) {
+    let result;
+    if (initialVal) {
+        result = initialVal
+        for (let i = 0; i < arr.length; i++) {
+            result = callback(arr[i],result)
+        }
+    } else {
+        result = arr[0]
+        for (let i = 1; i < arr.length; i++) {
+            result = callback(arr[i], result)
+        }
+    }
+    return result
+  }
